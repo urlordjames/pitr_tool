c48fe5040b1b497d1db5bc77c0fbaa2dee461b4f
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-02-08 15:56:02 -0500

    update cargo and bump version to 1.0.0

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let pkgs = import <nixpkgs> {};
 in pkgs.rustPlatform.buildRustPackage {
 	pname = "pitr_tool";
-	version = "0.1.0";
+	version = "1.0.0";
 
 	src = ./.;
 
-	cargoSha256 = "0i95cjfn947anprjzflq8m24qplwcf8ya98hrhns8k5k6syv0km9";
+	cargoSha256 = "0j13ky96qy9drp7v8mpki2r3amm83cxx02afnz66k3s6qjqpivy7";
 }
