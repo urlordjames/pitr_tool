d65be4f4658c34c485395ef1998c48b2018f91f3
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-02-16 16:38:54 -0500

    use git tag for pitr_util

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let pkgs = import <nixpkgs> {};
 in pkgs.rustPlatform.buildRustPackage {
 	pname = "pitr_tool";
-	version = "1.0.0";
+	version = "1.0.1";
 
 	src = ./.;
 
-	cargoSha256 = "0j13ky96qy9drp7v8mpki2r3amm83cxx02afnz66k3s6qjqpivy7";
+	cargoSha256 = "1w3z6qhly2f1lm7irh1xqrp6afzh2g9fd96784lk9qyzvknxa815";
 }
